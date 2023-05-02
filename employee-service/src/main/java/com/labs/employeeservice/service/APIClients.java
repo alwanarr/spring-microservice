dec1d9a18177d998d33163ea9f8b74b6cbda5a22
alwanarfianto <alwanarfianto@gmail.com>
2023-05-02 14:57:46 +0700

    Menambahkan Service Registry dan Discovery. konfigurasi multi instances

--- employee-service/src/main/java/com/labs/employeeservice/service/APIClients.java
+++ employee-service/src/main/java/com/labs/employeeservice/service/APIClients.java
@@ -2,12 +2,11 @@ package com.labs.employeeservice.service;
 
 import com.labs.employeeservice.dto.DepartmentDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClients {
 
     @GetMapping("/api/departments/{department-code}")
